e34c20f8d6b461b19ac207793fdf9b4c852ee4a0
Bazelsneeze <pcleary2006@icloud.com>
2022-10-15 09:30:08 -0700

    toes

--- src/main/java/net/tntninja2/dontdie/block/ModBlocks.java
+++ src/main/java/net/tntninja2/dontdie/block/ModBlocks.java
@@ -1,5 +1,5 @@
 package net.tntninja2.dontdie.block;
-
+//Test
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
